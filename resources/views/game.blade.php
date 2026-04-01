fd692efa3e1907fab8caf732ad5cd7b402be3a9a
Mainalam7084 <158204555+Mainalam7084@users.noreply.github.com>
2026-04-01 12:06:06 +0200

    Añadir estilos al game

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,68 +1,64 @@
 <?php
-require(app_path() . '/Includes/steam_wrapper.php');
+require app_path() . '/Includes/steam_wrapper.php';
 
-$appid = $_GET["appid"] ?? null;
-if ($appid == null)
-	die("No appid");
+$appid = $_GET['appid'] ?? null;
+if ($appid == null) {
+    die('No appid');
+}
 
-$details = getAppDetails($appid, "es")[$appid]["data"];
-$app_name = $details["name"];
-$app_short_desc = $details["short_description"];
-$app_header_img = $details["header_image"];
-$app_price = $details["price_overview"]["final_formatted"] ?? "Free";
-$app_publisher = $details["publishers"][0] ?? "Unknown";
-$app_developer = $details["developers"][0] ?? "Unknown";
-$app_detailed_desc = $details["detailed_description"] ?? "";
+$details = getAppDetails($appid, 'es')[$appid]['data'];
+$app_name = $details['name'];
+$app_short_desc = $details['short_description'];
+$app_header_img = $details['header_image'];
+$app_price = $details['price_overview']['final_formatted'] ?? 'Free';
+$app_publisher = $details['publishers'][0] ?? 'Unknown';
+$app_developer = $details['developers'][0] ?? 'Unknown';
+$app_detailed_desc = $details['detailed_description'] ?? '';
 
-$discount_percent = $details["price_overview"]["discount_percent"];
-$discount_formatted = $discount_percent > 0 ? "(-$discount_percent%)" : "";
+$discount_percent = $details['price_overview']['discount_percent'] ?? 0;
+$discount_formatted = $discount_percent > 0 ? "(-$discount_percent%)" : '';
 
-$screenshots = $details["screenshots"];
+$screenshots = $details['screenshots'];
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-	<link rel="stylesheet" href="../css/style.css">
-	<link rel="stylesheet" href="../css/game.css">
-	<link rel="stylesheet" href="../css/slideshow.css">
-	<script src="../js/slideshow.js"></script>
-	<script src="../js/jquery.js"></script>
-	<title><?= $app_name ?></title>
+    @vite(['resources/css/style.css', 'resources/css/game.css', 'resources/css/slideshow.css', 'resources/js/slideshow.js'])
+    <title><?= $app_name ?></title>
 </head>
 
 <body>
-	<header>
-		<a href="../">Volver</a>
-	</header>
-	<div class="gameContainer">
-		<h1><?= $app_name; ?></h1>
-		<img src="<?= $app_header_img ?>" alt="<?= $app_name ?>">
+    <header>
+        <a href="../">Volver</a>
+    </header>
+    <div class="gameContainer">
+        <h1><?= $app_name ?></h1>
+        <img src="<?= $app_header_img ?>" alt="<?= $app_name ?>">
 
-		<div class="info">
-			<p><strong>Price:</strong> <?= $app_price; ?> <?= $discount_formatted ?></p>
-			<p><strong>Publisher:</strong> <?= $app_publisher; ?></p>
-			<p><strong>Developer:</strong> <?= $app_developer; ?></p>
-		</div>
+        <div class="info">
+            <p><strong>Price:</strong> <?= $app_price ?> <?= $discount_formatted ?></p>
+            <p><strong>Publisher:</strong> <?= $app_publisher ?></p>
+            <p><strong>Developer:</strong> <?= $app_developer ?></p>
+        </div>
 
-		<p class="short-description"><?= $app_short_desc; ?></p>
-		<div class="slideshow-container">
-			<?php for ($i = 0; $i < count($screenshots); $i++): ?>
-				<div class="mySlides">
-					<img style="width:100%" src="<?= $screenshots[$i]['path_thumbnail'] ?>">
-					<div class="numbertext"><?= $i+1 ?> / <?= count($screenshots) ?></div>
-				</div>
-			<?php endfor; ?>
+        <p class="short-description"><?= $app_short_desc ?></p>
+        <div class="slideshow-container">
+            <?php for ($i = 0; $i < count($screenshots); $i++): ?>
+            <div class="mySlides">
+                <img style="width:100%" src="<?= $screenshots[$i]['path_thumbnail'] ?>">
+                <div class="numbertext"><?= $i + 1 ?> / <?= count($screenshots) ?></div>
+            </div>
+            <?php endfor; ?>
 
-			<a class="prev" onclick="plusSlides(-1)">❮</a>
-			<a class="next" onclick="plusSlides(1)">❯</a>
+            <a class="prev" onclick="plusSlides(-1)">❮</a>
+            <a class="next" onclick="plusSlides(1)">❯</a>
 
-		</div>
-		<div class="detailed-description">
-			<?= $app_detailed_desc; ?>
-		</div>
-	</div>
+        </div>
+        <div class="detailed-description">
+            <?= $app_detailed_desc ?>
+        </div>
+    </div>
 </body>
 
 </html>
-
